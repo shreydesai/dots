1f4836b9e582ac2daa6855e2882951982995784b
Shrey Desai <shreydesai@me.com>
2016-04-26 17:21:18 -0700

    added documentation for the enemy classes

--- model/GreenEnemy.java
+++ model/GreenEnemy.java
@@ -3,15 +3,29 @@ package model;
 import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
 
+/**
+ * Represents the Green Enemy - it is a child of the
+ * Enemy abstract class
+ */
 public class GreenEnemy extends Enemy {
     
     private static final int POINTS = 10;
     private static final int DELTA = 2;
     
+    /**
+     * Default constructor
+     */
     public GreenEnemy() {
         
     }
-
+    
+    /**
+     * Constructor
+     * @param pane setting of the game
+     * @param cx center X position of the enemy
+     * @param cy center Y position of the enemy
+     * @param r radius of the enemy
+     */
     public GreenEnemy(Pane pane, double cx, double cy, double r) {
         super(pane, cx, cy, r);
         this.getNode().getStyleClass().add("green-enemy");
@@ -19,17 +33,26 @@ public class GreenEnemy extends Enemy {
     }
     
     @Override
+    /**
+     * Starts the animation loop of the enemy
+     */
     public void start() {
         new AnimationTimer() {
 
             @Override
             public void handle(long now) {
+                // Stop the timer if the pane no longer contains
+                // the node element of the enemy
                 if (!pane.getChildren().contains(circle)) {
                     this.stop();
                 }
+                // Prevents the node element of the enemy
+                // from going vertically off of the canvas
                 if (getY() + getR() + DELTA >= HEIGHT) {
                     setY(0);
                 }
+                // Move the Y position of the node element of the
+                // enemy by adding the DELTA variable
                 setY(getY() + DELTA);
             }
             
@@ -37,11 +60,18 @@ public class GreenEnemy extends Enemy {
     }
 
     @Override
+    /**
+     * Getter method for the points the enemy is worth
+     * @return number of points
+     */
     public int getPoints() {
         return POINTS;
     }
     
     @Override
+    /**
+     * Getter method for a string representation
+     */
     public String toString() {
         return "Green Enemy";
     }
--- model/RedEnemy.java
+++ model/RedEnemy.java
@@ -2,14 +2,28 @@ package model;
 
 import javafx.scene.layout.Pane;
 
+/**
+ * Represents the Red Enemy - it is a child of the
+ * Enemy abstract class
+ */
 public class RedEnemy extends Enemy {
     
     private static final int POINTS = 5;
     
+    /**
+     * Default constructor
+     */
     public RedEnemy() {
         
     }
-
+    
+    /**
+     * Constructor
+     * @param pane setting of the game
+     * @param cx center X position of the enemy
+     * @param cy center Y position of the enemy
+     * @param r radius of the enemy
+     */
     public RedEnemy(Pane pane, double cx, double cy, double r) {
         super(pane, cx, cy, r);
         this.getNode().getStyleClass().add("red-enemy");
@@ -17,16 +31,26 @@ public class RedEnemy extends Enemy {
     }
     
     @Override
+    /**
+     * Starts the animation loop of the enemy
+     */
     public void start() {
         return;
     }
 
     @Override
+    /**
+     * Getter method for the points the enemy is worth
+     * @return number of points
+     */
     public int getPoints() {
         return POINTS;
     }
     
     @Override
+    /**
+     * Getter method for a string representation
+     */
     public String toString() {
         return "Red Enemy";
     }
