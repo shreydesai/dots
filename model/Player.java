97d2d8443e5477ed684dea1bbe0e4ed4e9759096
Shrey Desai <shreydesai@me.com>
2016-05-23 13:15:21 -0700

    wrote documentation for Player class

--- model/Player.java
+++ model/Player.java
@@ -2,6 +2,10 @@ package model;
 
 import javafx.scene.shape.Circle;
 
+/**
+ * Represents the Player in the game - it implements the
+ * Sprite interface for its functionalities
+ */
 public class Player implements Sprite {
     
     private final static int WIDTH = 1000;
@@ -11,17 +15,30 @@ public class Player implements Sprite {
     private double cx, cy, r;
     private Circle circle;
     
+    /**
+     * Constructor
+     * @param cx center x position
+     * @param cy center y position
+     * @param r radius of the circle node
+     */
     public Player(double cx, double cy, double r) {
         this.cx = cx;
         this.cy = cy;
         this.r = r;
         
+        // Instantiates a new Circle node to represent the
+        // Player on the canvas
         circle = new Circle(this.cx, this.cy, this.r);
     }
     
     @Override
+    /**
+     * Moves the Player in the north direction
+     */
     public void moveUp() {
         if (getY() - getR() - DELTA < 0) {
+            // Disable movement if the Player has reached
+            // the top of the canvas
             return;
         } else {
             setY(getY() - DELTA);
@@ -29,8 +46,13 @@ public class Player implements Sprite {
     }
     
     @Override
+    /**
+     * Moves the Player in the south direction
+     */
     public void moveDown() {
         if (getY() + getR() + DELTA > HEIGHT) {
+            // Disable movement if the Player has reached
+            // the bottom of the canvas
             return;
         } else {
             setY(getY() + DELTA);
@@ -38,8 +60,13 @@ public class Player implements Sprite {
     }
     
     @Override
+    /**
+     * Moves the Player in the west direction
+     */
     public void moveLeft() {
         if (getX() - getR() - DELTA < 0) {
+            // Disable movement if the Player has reached
+            // the west side of the canvas
             return;
         } else {
             setX(getX() - DELTA);
@@ -47,8 +74,13 @@ public class Player implements Sprite {
     }
     
     @Override
+    /**
+     * Moves the Player in the east direction
+     */
     public void moveRight() {
         if (getX() + getR() + DELTA > WIDTH) {
+            // Disable movement if the Player has reached
+            // the east side of the canvas
             return;
         } else {
             setX(getX() + DELTA);
@@ -56,31 +88,53 @@ public class Player implements Sprite {
     }
     
     @Override
+    /**
+     * Getter method for the Player element
+     * @return circle node
+     */
     public Circle getNode() {
         return circle;
     }
     
     @Override
+    /**
+     * Getter method for the Player's radius
+     * @return circle node radius
+     */
     public double getR() {
         return circle.getRadius();
     }
     
     @Override
+    /**
+     * Getter method for the Player's X coordinate
+     * @return circle x coordinate
+     */
     public double getX() {
         return circle.getCenterX();
     }
     
     @Override
+    /**
+     * Getter method for the Player's Y coordinate
+     * @return circle y coordinate
+     */
     public double getY() {
         return circle.getCenterY();
     }
     
     @Override
+    /**
+     * Sets the Player's X coordinate
+     */
     public void setX(double cx) {
         circle.setCenterX(cx);
     }
     
     @Override
+    /**
+     * Sets the Player's Y coordinate
+     */
     public void setY(double cy) {
         circle.setCenterY(cy);
     }
